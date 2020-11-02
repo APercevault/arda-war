edf40ac4e53f4f162feafb585d3cc3ab73a80927
Chmout <56177099+Chmout@users.noreply.github.com>
2020-11-03 00:59:46 +0100

    test

--- test.php
+++ test.php
@@ -14,6 +14,10 @@
 
 <body>
     <h1>Bonjour</h1>
+    <?php 
+    echo "ICI C'EST PHP LA CON DE TES MORTS !"
+    
+    ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
